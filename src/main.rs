c8cacbcc94baca507441c20432e197e4aa0427e9
Kerollmops <clement@meilisearch.com>
2023-08-10 00:34:51 +0200

    Generate articles and links

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,66 @@
-use std::fs::{self, File};
-use std::io::{self, ErrorKind, Write};
-
 use askama::Template;
+use big_s::S;
+use octocrab::params::State;
+use octocrab::{format_media_type, OctocrabBuilder};
+use tokio::fs::{self, File};
+use tokio::io::{self, ErrorKind};
 
 #[derive(Template)]
 #[template(path = "index.html")]
-struct IndexTemplate<'a> {
-    name: &'a str,
+struct IndexTemplate {
+    title: String,
+    articles: Vec<ArticleInList>,
 }
 
-fn main() -> anyhow::Result<()> {
-    fs::remove_dir_all("output").or_else(ignore_not_found)?;
-    fs::create_dir("output")?;
+struct ArticleInList {
+    title: String,
+    url: String,
+}
 
-    let mut index_file = File::create("output/index.html")?;
-    let index = IndexTemplate { name: "world" };
+#[derive(Template)]
+#[template(path = "article.html", escape = "none")]
+struct ArticleTemplate {
+    title: String,
+    html_content: String,
+}
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let owner_repo = std::env::var("GITHUB_REPOSITORY").expect("please define `GITHUB_REPOSITORY`");
+    let (owner, repo) = owner_repo.split_once('/').unwrap();
+
+    fs::remove_dir_all("output").await.or_else(ignore_not_found)?;
+    fs::create_dir("output").await?;
+
+    // force GitHub to return HTML content
+    let octocrab = OctocrabBuilder::default()
+        .add_header(http::header::ACCEPT, format_media_type("html"))
+        .build()?;
+
+    let page = octocrab
+        .issues(owner, repo)
+        .list()
+        .state(State::Open)
+        .labels(&[S("article")])
+        .per_page(50)
+        .send()
+        .await?;
+
+    let mut articles = Vec::new();
+    for issue in page {
+        articles
+            .push(ArticleInList { title: issue.title.clone(), url: format!("{}", issue.number) });
+
+        let mut article_file =
+            File::create(format!("output/{}.html", issue.number)).await?.into_std().await;
+        let article =
+            ArticleTemplate { title: issue.title, html_content: issue.body_html.unwrap() };
+        article.write_into(&mut article_file)?;
+    }
+
+    let mut index_file = File::create("output/index.html").await?.into_std().await;
+    let index = IndexTemplate { title: S("Kerollmops' blog"), articles };
     index.write_into(&mut index_file)?;
-
-    // iterate over all the issue articles and generate the files.
 
     Ok(())
 }
